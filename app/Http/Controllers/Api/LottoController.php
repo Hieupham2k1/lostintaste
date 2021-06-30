ffc2cfd087c849202ac456c2096d751c0a104b1f
Hiếu Phạm <trunghie01@gmail.com>
2021-06-30 12:49:24 +0700

    lotto api

--- app/Http/Controllers/Api/LottoController.php
+++ app/Http/Controllers/Api/LottoController.php
@@ -9,7 +9,13 @@ class LottoController extends Controller
 {
     public function index($year, $month, $date = null)
     {
-        if($date) return $this->getStaticOfDate($date, $month, $year);
+        if($date){
+            $handle = curl_init();
+            $result = $this->getStaticOfDate($date, $month, $year, $handle);
+            curl_close($handle);
+            return $result;
+        }
+        
         return $this->getStaticOfMonth($month, $year);
     }
 
@@ -22,14 +28,19 @@ class LottoController extends Controller
         return substr($string, $ini, $len);
     }
 
-    function getStaticOfDate($day, $month, $year){
+    function getStaticOfDate($day, $month, $year, $handle){
         $static = [
             'de' => null,
             'lo' => [],
         ];
 
         try{
-            $html  = htmlentities(file_get_contents("https://xosodaiphat.com/xsmb-$day-$month-$year.html"));
+            $url = "https://xosodaiphat.com/xsmb-$day-$month-$year.html";
+            
+            curl_setopt($handle, CURLOPT_URL, $url);
+            curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+            
+            $html = curl_exec($handle);
         } catch(Exception $e){
             return $static;
         }
@@ -37,7 +48,7 @@ class LottoController extends Controller
         $array = explode('span', $table);
         
         foreach($array as $index => $a){
-            $number = $this->get_string_between($a, '&gt;', '&lt;');
+            $number = $this->get_string_between($a, '>', '<');
             if(is_numeric($number)){
                 $length = strlen($number);
                 $lastTwoDigit = substr($number, $length - 2, $length);
@@ -61,10 +72,14 @@ class LottoController extends Controller
         ];
         $now = now();
         if($month > $now->month || $year > $now->year) return $static;
+
         $isCurrentMonth = $year == $now->year && $month == $now->month;
+        $handle = curl_init();
         for($date = 1; $date <= $maxDateOfMonth[(int) $month]; $date++){
             if($isCurrentMonth && $date >= $now->day) break;
-            $dateStatic = $this->getStaticOfDate($date, $month, $year);
+
+            $dateStatic = $this->getStaticOfDate($date, $month, $year, $handle);
+
             if($dateStatic['de']){
                 if(!isset($static['de'][$dateStatic['de']])) $static['de'][$dateStatic['de']] = 0;
                 $static['de'][$dateStatic['de']]++;
@@ -74,6 +89,7 @@ class LottoController extends Controller
                 $static['lo'][$lo] += $count;
             }
         }
+        curl_close($handle);
         return $static;
     }
 }
